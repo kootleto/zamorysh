46617620afcc6263d7b3730beb91d900d9fb7d5c
kootleto <mit.pdf@gmail.com>
2026-03-04 00:02:44 +0300

    Extract logs to API, update log messages

--- resolver_api.py
+++ resolver_api.py
@@ -1,13 +1,14 @@
 from collections import defaultdict
 
-import ui_api
+import tasks_api
+import log_api
 
 
 def resolve_intents(gs):
     grouped_intents = defaultdict(list)
     for intent in gs["intents"]:
         grouped_intents[intent["type"]].append(intent)
-    ui_api.log("Raw Intents: ", dict(grouped_intents), log_type="resolver")
+    # log_api.log("Raw Intents: ", dict(grouped_intents), log_type="resolver")
     for intent_type, intents in grouped_intents.items():
         resolver_name = f"resolve_{intent_type}s"
         if resolver_name in globals():
@@ -30,7 +31,7 @@ def resolve_generic(gs, intents, gs_key, set_fn=max, mod_fn=sum, clamp_fn=lambda
             value = gs[gs_key][target]
         value += mod_fn([i["value"] for i in grouped["mod"]])
         value = clamp_fn(value)
-        ui_api.log(f"{target} {gs[gs_key][target]} -> {value}", log_type="resolver")
+        log_api.log(f"{target} {gs[gs_key][target]} -> {value}", log_type="resolver")
         gs[gs_key][target] = value
 
 
@@ -64,3 +65,22 @@ def resolve_flags(gs, intents):
         set_fn=any,
         clamp_fn=lambda v: v,
     )
+
+
+# WORK IN PROGRESS
+def resolve_tasks(gs, intents):
+    grouped_intents = defaultdict(lambda: {"mod": [], "set": []})
+    for intent in intents:
+        grouped_intents[intent["target"]][intent["op"]].append(intent)
+    for target, grouped in grouped_intents.items():
+        task = tasks_api.get_task_by_id(gs, target)
+        value = tasks_api.get_progress(task)
+        value += sum([i["value"] for i in grouped["mod"]])
+        value = min(value, tasks_api.get_required(task))
+
+        log_api.log(
+            f"Task {target} progress {tasks_api.get_progress(task)} -> {value} / {tasks_api.get_required(task)}",
+            log_type="resolver",
+        )
+
+        tasks_api.set_progress(task, value)
