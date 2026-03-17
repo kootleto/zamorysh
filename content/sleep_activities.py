a4819ffcb7b402edc36f85c47807d7e2ef5d62d9
astamarina <aksinya.tamarina@gmail.com>
2026-03-17 20:57:47 +0300

    Update sleep

--- content/sleep_activities.py
+++ content/sleep_activities.py
@@ -2,20 +2,21 @@ from engine import activities_api, gs_api
 from interface import ui
 
 
-def set_alarm(time):  # игрок должен ввести время будильника
-    time = ui.prompt("Установите время:")
+def set_alarm():  # игрок должен ввести время будильника
 
     def tick_effect(gs):
+        time = int(ui.prompt("Установите время: "))
         gs_api.set_timer(gs, "alarm", time)
 
-    return activities_api.base_activity(tick_effect, name="set_alarm")
+    return activities_api.base_activity(
+        tick_effect, hold_required=True, name="set_alarm"
+    )
 
 
 def sleep():
 
     def tick_effect(gs):
         gs_api.mod_vital(gs, "sleepiness", -2)  # 20 каждый час
-        gs_api.mod_vital(gs, "current_sleeping_time", 1)  # посчитать количество тиков
 
     def can_continue(gs):
         return (
@@ -26,18 +27,4 @@ def sleep():
     return activities_api.base_activity(tick_effect, can_continue, name="sleep")
 
 
-def get_up():
-    def tick_effect(gs):
-        gs_api.mod_vital(
-            gs, "sleeping_time", gs_api.get_vital(gs, "current_sleeping_time")
-        )
-        gs_api.mod_vital(
-            gs, "current_sleeping_time", -gs_api.get_vital(gs, "current_sleeping time")
-        )  # либо поменять значение sleeping_time на 0
-
-    def can_continue(gs):
-        return (
-            gs_api.get_vital(gs, "current_sleeping_time") > 0
-        )  # хочется чтобы запускалось только после сна...
-
-    return activities_api.base_activity(tick_effect, can_continue, name="get_up")
+activities = [set_alarm, sleep]
--- content/sleep_scenarios.py
+++ content/sleep_scenarios.py
@@ -1,7 +1,7 @@
 from engine import scenarios_api, gs_api
 
 
-def sleepiness_scenario():  # будет ли это каждый тик??
+def sleepiness_scenario():
     def check():
         return True
 
@@ -9,36 +9,8 @@ def sleepiness_scenario():  # будет ли это каждый тик??
         return gs_api.mod_vital(gs, "sleepiness", 1)  # 10 в час
 
     return scenarios_api.base_scenario(
-        scenarios_api.base_transition(0, 1, check, getting_sleepy)
+        [scenarios_api.base_transition(0, 0, check, getting_sleepy)]
     )
 
 
-def sleep_scenario():
-    def check_enough(gs):
-        return gs_api.get_vital(gs, "sleeping_time") >= 420  # 7 часов короче
-
-    def enough(gs):
-        if gs_api.get_vital(gs, "sleeping_debt") >= 20:
-            gs_api.mod_vital(gs, "sleeping_debt", -20)
-
-    def check_little(gs):
-        return 240 < gs_api.get_vital(gs, "sleeping_time") < 420
-
-    def little(gs):
-        return gs_api.mod_vital(gs, "sleeping_debt", 10)
-
-    def check_very_little(gs):
-        return gs_api.get_vital(gs, "sleeping_debt") <= 240
-
-    def very_little(gs):
-        return gs_api.mod_vital(gs, "sleeping_debt", 20)
-
-    return scenarios_api.base_scenario(
-        [
-            scenarios_api.base_transition(
-                0, 1, check_enough, enough
-            ),  # номер состояния??
-            scenarios_api.base_transition(1, 2, check_little, little),
-            scenarios_api.base_transition(2, 3, check_very_little, very_little),
-        ]
-    )
+scenarios = [sleepiness_scenario]
