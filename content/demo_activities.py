eb52785e5f8e37a1cf8503bd3006ea556bcbaab9
anna9312323269-arch <Anna.9312323269@gmail.com>
2026-04-04 22:07:16 +0300

    Copy paste

--- content/demo_activities.py
+++ content/demo_activities.py
@@ -1,8 +1,5 @@
 from engine import activities_api, gs_api, state_api
 
-from interface import ui
-import random
-
 
 # Параметрами активности может быть что угодно.
 # Вынос конкретных значений в параметры позволит легко менять их
@@ -12,7 +9,6 @@ import random
 def work(fatigue_cost=1, earn_money=1):
     def tick_effect(gs):
         gs_api.mod_vital(gs, "fatigue", +fatigue_cost)
-        # gs_api.mod_vital(gs, "mental", -mental_cost)
         gs_api.mod_stat(gs, "money", +earn_money)
 
     def can_continue(gs):
@@ -34,21 +30,6 @@ def rest():
 
 
 # Пример использования override_activity
-def pl(hold_required=False):
-
-    def tick_effect(gs, money_loss=-1000, money_play=-1):
-        if random.randint(1, 13) % 13 == 0:
-            print("Онет!")
-            gs_api.mod_stat(gs, "money", +money_loss)
-        else:
-            gs_api.mod_stat(gs, "money", +money_play)
-
-    def can_continue(gs):
-        return gs_api.get_stat(gs, "money") > 0
-
-    return activities_api.base_activity(can_continue, name="pl")
-
-
 def rest_hard():
     return activities_api.override_activity(
         rest(), hold_required=False, name="rest in peace"
@@ -126,79 +107,5 @@ def waste_money(param, state=None):
     )
 
 
-# if gs_api.get_time(gs) == 10:
-# if random.randint(1, 3) % 3 == 0:
-# gs_api.mod_vital(gs, "fatigue", +5.05)
-# if random.randint(1, 3) % 3 == 1:
-# gs_api.mod_vital(gs, "mental", +5.05)
-# if random.randint(1, 3) % 3 == 2:
-# gs_api.mod_stat(gs, "money", +5.05)
-
-
-def play_games(hold_required=False, fatigue_cost=2, earn_money=-1, loose_money=-3):
-    def tick_effect(gs):
-        # if random.randint(1, 2) % 2 == 0:
-        gs_api.mod_vital(
-            gs, "money", +loose_money
-        )  # вот так мы не делаем, короче идея в банкротстве
-        gs_api.mod_vital(gs, "fatigue", +fatigue_cost)
-        # else:
-        # gs_api.mod_vital(gs, "fatigue", +fatigue_cost)
-        # gs_api.mod_stat(gs, "money", +earn_money)
-
-    def can_continue(gs):
-        return gs_api.get_vital(gs, "fatigue") < 10
-
-    return activities_api.base_activity(
-        tick_effect, can_continue, hold_required, name="play_games"
-    )
-
-
-# def quote_kvest(hold_required=True, earn_money=1):
-# print("Нужно угадать цитату")
-# def tick_effect(gs):
-# if
-# gs_api.mod_stat(gs, "money", +earn_money)
-# print("")
-
-
-# return activities_api.base_activity(tick_effect, hold_required, name="quote_kvest")
-def random_event(
-    state=None,
-    hold_required=False,
-    # earn_money=random.randint(1, 2),
-    # fatigue_cost=random.randint(1, 5),
-    earn_money=1,
-    fatigue_cost=1,
-):
-    state = state_api.init_defaults(state, counter=1)
-
-    def tick_effect(gs):
-        state["counter"] -= 1
-        ran = random.randint(1, 3) % 3
-        if ran == 0:
-            ui.display("Вы нашли на дороге 5 рублей! Поздравляем!")
-            gs_api.mod_stat(gs, "money", +earn_money)
-        if ran == 1:
-            ui.display(
-                "Онет! Вы врезались в Ландера, пока опаздывали на дискру. Он расстроен."
-            )
-            gs_api.mod_vital(gs, "fatigue", -fatigue_cost)
-        else:
-            ui.display("Вы проспорили деньги на свой накоп по дискре (он меньше 3)")
-            gs_api.mod_stat(gs, "money", -earn_money)
-
-    def can_continue():
-        return state["counter"] > 0
-
-    return activities_api.base_activity(
-        hold_required=False,
-        tick_effect=tick_effect,
-        can_continue=can_continue,
-        name="random_event",
-    )
-
-
-activities = [work, rest, rest_hard, work_and_rest, cry, waste_money, random_event]
-# activities = [work, rest, rest_hard, work_and_rest, cry, waste_money]
+activities = [work, rest, rest_hard, work_and_rest, cry, waste_money]
 # Раскомментируйте эту строчку, чтобы добавить в игру демо-активности
