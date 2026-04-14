db70991adee3dd6ba2b58a7fa740650b2b58f462
kootleto <mit.pdf@gmail.com>
2026-04-15 00:03:50 +0300

    Add timed activities

---
+++ content/demo/demo_timed_activities.py
@@ -0,0 +1,41 @@
+from engine import state_api, activities_api
+from gameplay.activity_wrappers import timed_activity, single_tick_activity
+from interface import ui
+
+
+# Пример активности с ограничением по времени
+# Эта активность будет длиться всего пять тиков. Теперь не надо вручную прописывать счетчик!
+def display_numbers_for_10_ticks(state=None):
+    state = state_api.init_defaults(state, number=2)
+
+    def tick_effect():
+        ui.display(state["number"])
+        state["number"] *= 2
+
+    def can_continue():
+        return True
+
+    # Обратите внимание: мы передаем в обертку уже готовую активность,
+    # а не ее кусочки вроде tick_effect или can_continue
+    return timed_activity(
+        activities_api.base_activity(
+            tick_effect, can_continue, name="отображать рандомные числа"
+        ),
+        state,
+        duration=5,
+    )
+
+
+# Пример однотиковой активности
+# Однотиковые активности используют под капотом такой же счетчик, что и обычные timed_activities
+# Хотя здесь мы не используем state внутри основной активности, он ей всё равно нужен для этого счетчика
+def display_hello_world(state=None):
+    def tick_effect():
+        ui.display("Hello world!")
+
+    return single_tick_activity(
+        activities_api.base_activity(tick_effect, name="сказать привет"), state
+    )
+
+
+activities = [display_numbers_for_10_ticks, display_hello_world]
