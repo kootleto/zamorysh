e47e4c6643ee78de33ceaf181a68df33c61dc6d1
astamarina <aksinya.tamarina@gmail.com>
2026-04-20 14:36:43 +0300

    Update locations

--- content/movements.py
+++ content/movements.py
@@ -1,24 +1,27 @@
 from engine import gs_api, activities_api
-from interface import ui
 from gameplay.api import location
 
 
-def move():
+@activities_api.with_param_space(location.get_directions)
+def move(param=None):
+
     def tick_effect(gs):
-        for i in range(len(location.get_directions(gs))):
-            ui.display(i, location.get_directions(gs)[i])
+        direction = param
+        if direction == "east":
+            location.mod(gs, location.y, 1)
+        elif direction == "west":
+            location.mod(gs, location.y, -1)
+        elif direction == "north":
+            location.mod(gs, location.x, 1)
+        elif direction == "south":
+            location.mod(gs, location.x, -1)
 
-        selected_direction = int(ui.prompt("Введите номер направления: "))
-        if location.get_directions(gs)[selected_direction] == "north":
-            location.mod(gs, location.ycoordinate, 1)
-        elif location.get_directions(gs)[selected_direction] == "south":
-            location.mod(gs, location.ycoordinate, -1)
-        elif location.get_directions(gs)[selected_direction] == "east":
-            location.mod(gs, location.xcoordinate, 1)
-        elif location.get_directions(gs)[selected_direction] == "west":
-            location.mod(gs, location.xcoordinate, -1)
+    def can_continue(gs):
 
-    def can_continue():
-        return True
 
-    return activities_api.base_activity(tick_effect, can_continue, True, name="move")
+    return activities_api.base_activity(
+tick_effect, can_continue, name=f"move to {param}"
+    )
+
+
+activities = [move]
--- gameplay/api/location.py
+++ gameplay/api/location.py
@@ -1,31 +1,41 @@
 from engine import gs_api
-from engine.resolver_api import resolve_generic
+from collections import defaultdict
+from typing import Callable, Iterable, Any
+from engine.schema import GameState, Intent, Resolver
+from tools.logger import log
 
 domain = "location"
 
-xcoordinate = "xcoordinate"
-ycoordinate = "ycoordinate"
+x = "xcoordinate"
+y = "ycoordinate"
 # coordinates = (xcoordinate, ycoordinate)
 place = ""
 
 
 initial = {
-    xcoordinate: 0,
-    ycoordinate: 0,
+    x: 0,
+    y: 0,
 }
 
 
-def resolve(gs, intents):
-    resolve_generic(
-        gs,
-        intents,
-        domain,
-        set_fn=max,  # ???
-        mod_fn=sum,
-        clamp_fn=lambda v: max(
-            0, min(100, v)
-        ),  # а мне надо х от 0 до 60, а у от 0 до 5
-    )
+def resolve_intents(
+    gs: GameState,
+    intents: Iterable[Intent],
+    domain: str,
+    set_fn: Callable[[Iterable[Any]], Any] = max,
+    mod_fn: Callable[[Iterable[Any]], Any] = sum,
+    clamp_fn: Callable = lambda x: x,
+):
+    grouped_intents = defaultdict(lambda: {"set": [], "mod": []})
+    for intent in intents:
+        grouped_intents[intent["target"]][intent["op"]].append(intent)
+    for target, grouped in grouped_intents.items():
+        if grouped["set"]:
+            value = set_fn([i["value"] for i in grouped["set"]])
+        else:
+            value = gs["gameplay"][domain][target]
+        value += mod_fn([i["value"] for i in grouped["mod"]])
+        if
 
 
 def get(gs, coordinate):
@@ -42,31 +52,31 @@ def mod(gs, coordinate, delta):
 
 def get_directions(gs):
     directions = []
-    if get(gs, xcoordinate) < 60:
+    if get(gs, x) < 60:
         directions.append("east")
-    if get(gs, xcoordinate) > 0:
+    if get(gs, x) > 0:
         directions.append("west")
-    if get(gs, ycoordinate) < 5:
+    if get(gs, y) < 5:
         directions.append("north")
-    if get(gs, ycoordinate) > 0:
+    if get(gs, y) > 0:
         directions.append("south")
     return directions
 
 
 def get_place(gs):
-    if get(gs, xcoordinate) == 0 and get(gs, ycoordinate) == 0:
+    if get(gs, x) == 0 and get(gs, y) == 0:
         mod(gs, place, "home")
-    elif 0 <= get(gs, xcoordinate) < 11 and get(gs, ycoordinate) == 5:
+    elif 0 <= get(gs, x) < 11 and get(gs, y) == 5:
         mod(gs, place, "park")
-    elif 10 < get(gs, xcoordinate) < 40:
+    elif 10 < get(gs, x) < 40:
         mod(gs, place, "metro")
-    elif get(gs, xcoordinate) == 60 and get(gs, ycoordinate) == 0:
+    elif get(gs, x) == 60 and get(gs, y) == 0:
         mod(gs, place, "university")
-    elif (get(gs, xcoordinate) == 10 or get(gs, xcoordinate) == 60) and get(
-        gs, ycoordinate
+    elif (get(gs, x) == 10 or get(gs, x) == 60) and get(
+        gs, y
     ) == 5:
         mod(gs, place, "coffee house")
-    elif get(gs, xcoordinate) == 40 and get(gs, ycoordinate) == 5:
+    elif get(gs, x) == 40 and get(gs, y) == 5:
         mod(gs, place, "club")
     else:
         mod(gs, place, "outside")
