789e7b61442a1af6e21850d2585af6c8986d1cc7
anna9312323269-arch <Anna.9312323269@gmail.com>
2026-04-28 23:17:46 +0300

    Fix random scenarios

--- content/random_scenarios.py
+++ content/random_scenarios.py
@@ -35,31 +35,31 @@ def random_scenario2(state=None):
         return gs_api.get_time(gs) == state["tick"]
 
     def eff(gs):
-        r = random.randint(1, 100)
+        r = random.randint(1, 100) # У событий не одинаковые вероятности, потому что в жизни так не бывает.
         if r % 6 == 0:
             stats.mod(gs, "money", -5)
             ui.display(
                 "Вы оставили кошелек на Басмаче и оплатили проезд в метро деньгами."
             )
         if r % 6 == 1:
-            vitals.mod(gs, "sleepiness", +20)
+            vitals.mod(gs, vitals.sleepiness, +20)
             ui.display(
                 "Одногруппник, сидящий рядом с вами на семинаре, зевнул. Вы тоже зевнули и захотели спать."
             )
         if r % 6 == 2:
-            vitals.mod(gs, "mental", -30)
+            vitals.mod(gs, vitals.mental, -30)
             ui.display(
                 "Онет. Вы увидели свою оценку за домашку по Линде. Она меньше 4,7."
             )
         if r % 6 == 3:
-            stats.mod(gs, "knowledge", +5)
+            stats.mod(gs, stats.knowledge, +5)
             ui.display(
                 "Вы искали что-то в Википедии и случайно перешли по ссылке на статью языка тигринья. Вы чувствуете себя умнее."
             )
         if r % 6 == 4:
             ui.display("ВЫ ПРОИГРАЛИ")
         if r % 6 == 5:
-            stats.mod(gs, "social", -10)
+            stats.mod(gs, stats.social, -10)
             ui.display("Вам пришлось говорить о СОПе с одногруппниками. Это тяжело.")
 
     return scenarios_api.base_scenario(
