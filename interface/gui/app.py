4888251b9ed62b2ad570ee6ce7ddd6e71cb05f8d
kootleto <mit.pdf@gmail.com>
2026-05-06 16:45:37 +0300

    Fix

--- interface/gui/app.py
+++ interface/gui/app.py
@@ -74,8 +74,7 @@ class GameApp(App):
 
         pygame.mixer.music.load(path)
         pygame.mixer.music.set_volume(self.vs["volume"] / 100)
-        pygame.mixer.music.play(loops=-1)
-        pygame.mixer.music.set_pos(0)
+        Clock.schedule_once(lambda dt: pygame.mixer.music.play(loops=-1), 0)
 
     def _on_key_down(self, _window, key, *_args):
         if key == 13:
