6797c0ba724242e4513b310676eef74ebc026de6
astamarina <aksinya.tamarina@gmail.com>
2026-05-06 16:32:57 +0300

    Update moving inside hse scenarios

--- content/move_inside_scenarios.py
+++ content/move_inside_scenarios.py
@@ -12,6 +12,10 @@ def floor_scenario(floor_number):
 
     return scenarios_api.base_scenario(
         [
+            scenarios_api.base_transition(0, "in", check, lambda: None),
+            scenarios_api.base_transition(
+                0, "out", lambda gs: not check(gs), lambda: None
+            ),
             scenarios_api.base_transition("out", "in", check, enter),
             scenarios_api.base_transition(
                 "in", "out", lambda gs: not check(gs), lambda: None
@@ -40,55 +44,10 @@ def fifth_floor():
     return floor_scenario(5)
 
 
-def get_classrooms(floor_number):
-    def check(gs):
-        return (
-            floors.get(gs, floors.FLOOR) == floor_number
-            and floors.get(gs, floors.CLASSROOM) == 0
-        )
-
-    def show_classrooms():
-        ui.display(f"Аудитории: {", ".join(floors.CLASSROOMS[floor_number])}")
-
-    return scenarios_api.base_scenario(
-        [
-            scenarios_api.base_transition("out", "in", check, show_classrooms),
-            scenarios_api.base_transition(
-                "in", "out", lambda gs: not check(gs), lambda: None
-            ),
-        ]
-    )
-
-
-def first_floor_classrooms():
-    return get_classrooms(1)
-
-
-def second_floor_classrooms():
-    return get_classrooms(2)
-
-
-def third_floor_classrooms():
-    return get_classrooms(3)
-
-
-def fourth_floor_classrooms():
-    return get_classrooms(4)
-
-
-def fifth_floor_classrooms():
-    return get_classrooms(5)
-
-
 SCENARIOS = [
     first_floor,
     second_floor,
     third_floor,
     fourth_floor,
     fifth_floor,
-    first_floor_classrooms,
-    second_floor_classrooms,
-    third_floor_classrooms,
-    fourth_floor_classrooms,
-    fifth_floor_classrooms,
 ]
