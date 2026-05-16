eacc0c203ead5b8addb45de55c5ce00c15f197e2
kootleto <mit.pdf@gmail.com>
2026-05-17 01:15:34 +0300

    Make explicit dotenv path

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()
+current_dir = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(current_dir, ".env")
+
+load_dotenv(dotenv_path=dotenv_path)
 
 
 def _add_bool_setting(parser, env_name, cmd_name, short_cmd_name, attr, help_text):
