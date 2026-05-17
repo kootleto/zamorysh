1a30837a6fb5bc3408ad07a6f5dd0ee7cb9bbe5b
astamarina <aksinya.tamarina@gmail.com>
2026-05-17 19:13:40 +0300

    Update

--- content/study_scenarios.py
+++ content/study_scenarios.py
@@ -1,7 +1,20 @@
-from gameplay.api import schedule
+from gameplay.api import schedule, vitals
+from random import randint, choice
+from interface import ui
 
 
 def study_scenario():
     def check(gs):
         return schedule.get_current_lesson(gs)["subject"] is not None
     def study(gs):
+        if schedule.get_current_lesson(gs)["type"] == "лекция":
+            # активность стади
+            if choice([True, False]):
+                ui.display('Ура, тема интересная!')
+                vitals.mod(gs, vitals.MENTAL, 5)
+            else:
+                ui.display('Эх, тема скучная...')
+                vitals.mod(gs, vitals.MENTAL, -5)
+
+        else:
+            
