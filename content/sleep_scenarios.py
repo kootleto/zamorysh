fb3025bf7bb57b3b2e0608647ae7fed4a6fb810b
astamarina <aksinya.tamarina@gmail.com>
2026-05-18 20:30:09 +0300

    Update scenario

--- content/sleep_scenarios.py
+++ content/sleep_scenarios.py
@@ -1,8 +1,10 @@
+from content import auto_start_manager
+from engine import scenarios_api
 from gameplay.api import vitals
 from interface import ui
 
 
-def falling_asleep():
+def falling_asleep(activity_definitions):
     def check_sleepy(gs):
         return vitals.get(gs, vitals.SLEEPINESS) >= 80
 
@@ -12,5 +14,16 @@ def falling_asleep():
     def check_very_sleepy(gs):
         return vitals.get(gs, vitals.SLEEPINESS) == 100
 
-    # def sleep(gs):
+    def auto_sleep():
+        auto_start_manager.auto_start_manager(activity_definitions)
 
+    return scenarios_api.base_scenario(
+        [
+            scenarios_api.base_transition(0, 1, check_sleepy, go_to_sleep),
+            scenarios_api.base_transition(1, 0, lambda: not check_sleepy, lambda: None),
+            scenarios_api.base_transition(1, 2, check_very_sleepy, auto_sleep),
+            scenarios_api.base_transition(
+                2, 1, lambda: not check_very_sleepy, lambda: None
+            ),
+        ]
+    )
