dc8ff71b2e59c6afec8d2ede5fb841b0827b63b3
anna9312323269-arch <Anna.9312323269@gmail.com>
2026-05-20 13:56:42 +0300

    fix it pls

--- content/movements.py
+++ content/movements.py
@@ -2,7 +2,7 @@ from engine import activities_api
 from gameplay.api import location, floors
 from gameplay.api.location import Place
 
-DIRECTIONS = {"north": (0, 1), "south": (0, -1), "east": (1, 0), "west": (-1, 0)}
+DIRECTIONS = {"север": (0, 1), "юг": (0, -1), "восток": (1, 0), "запад": (-1, 0)}
 
 
 @activities_api.with_param_space(location.get_directions)
@@ -27,7 +27,7 @@ def move(param=None):
         )
 
     return activities_api.base_activity(
-        tick_effect, can_continue, True, name=f"move to {param}"
+        tick_effect, can_continue, True, name=f"пойти на {param}"
     )
 
 
