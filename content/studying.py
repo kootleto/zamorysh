969e8577c320560f25059b555ef7845c06ed978f
astamarina <aksinya.tamarina@gmail.com>
2026-05-23 15:19:59 +0300

    Add homework

--- content/studying.py
+++ content/studying.py
@@ -1,12 +1,13 @@
 from engine import activities_api
 from engine.activities_api import override_activity
 from gameplay import productivity
-from gameplay.api import stats, schedule, floors
+from gameplay.api import stats, schedule, floors, vitals, location
 
 
 def study():
     def tick_effect(gs):
-        stats.mod(gs, stats.KNOWLEDGE, productivity.get(gs) * 0.1)
+        stats.mod(gs, stats.KNOWLEDGE, productivity.get(gs) * 0.25)
+        vitals.mod(gs, vitals.FATIGUE, -0.25)
 
     def can_continue(gs):
         return floors.get(gs, floors.CLASSROOM) == schedule.get_current_room(gs)
@@ -20,4 +21,20 @@ def auto_study():
     return override_activity(study(), hold_required=False, is_visible=False)
 
 
-ACTIVITIES = [study, auto_study]
+def do_homework():
+    def tick_effect(gs):
+        stats.mod(gs, stats.KNOWLEDGE, productivity.get(gs) * 0.25)
+        vitals.mod(gs, vitals.FATIGUE, -0.25)
+
+    def can_continue(gs):
+        return (
+            location.get_place(gs) == location.Place.HOME
+            or location.get_place(gs) == location.Place.UNIVERSITY
+        )
+
+    return activities_api.base_activity(
+        tick_effect, can_continue, hold_required=True, name="делать домашку"
+    )
+
+
+ACTIVITIES = [study, auto_study, do_homework]
