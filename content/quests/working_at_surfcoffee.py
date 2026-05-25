3a97a22cc4a8f100fc5c8b511884ed1320da8c5d
astamarina <aksinya.tamarina@gmail.com>
2026-05-25 15:20:19 +0300

    Update quest and fix schedule

--- content/quests/working_at_surfcoffee.py
+++ content/quests/working_at_surfcoffee.py
@@ -1,11 +1,12 @@
 import datetime
 import random
 
-from engine import activities_api, scenarios_api, gs_api
-from gameplay.api import stats, vitals, location, time
+from engine import activities_api, scenarios_api, gs_api, data_api
+from gameplay.api import stats, vitals, location, time, quests
 from interface import ui
 
 
+@activities_api.system_only
 def work():
 
     def tick_effect(gs):
@@ -18,6 +19,7 @@ def work():
         return (
             location.get_place(gs) == location.Place.SURF_COFFEE
             and 20 <= time.get_hour(gs) < 22
+            and quests.is_active(gs, "surf_coffee")
         )
 
     return activities_api.base_activity(
@@ -25,14 +27,17 @@ def work():
     )
 
 
-def coffee_quest(activity_definitions):
+def coffee_quest(activity_definitions, state=None):
+    state = data_api.init_defaults(state, working_days=0)
+
     def proposal_trigger(gs):
         return (
             time.get_day(gs) == time.START_DATETIME.day + 2
             and location.get_place(gs) == location.Place.SURF_COFFEE
         )
 
-    def proposal():
+    def proposal(gs):
+        quests.set_status(gs, "surf_coffee", quests.Status.ACTIVE)
         ui.display(
             "Вы видите объявление на кассе:\n'Нам срочно нужен бариста на вечерние смены."
             "\nТвоя задача - обслуживать покупателей с 20:00 до 22:00 три дня подряд."
@@ -43,7 +48,7 @@ def coffee_quest(activity_definitions):
 
     def check_start(gs):
         return (
-            time.get_day(gs) == time.START_DATETIME.day + 2
+            time.get_day(gs) == time.START_DATETIME.day + 3
             and location.get_place(gs) == location.Place.SURF_COFFEE
             and time.get_time(gs) == datetime.time(19, 50)
         )
@@ -62,13 +67,31 @@ def coffee_quest(activity_definitions):
     def start_working(gs):
         activities_api.start_activity_by_definition(gs, activity_definitions, work)
 
+    def check_absence(gs):
+        return location.get_place(gs) != location.Place.SURF_COFFEE and time.get_time(
+            gs
+        ) == datetime.time(20, 0)
+
+    def fire(gs):
+        ui.display(
+            "Вам приходит сообщение от шеф-баристы: 'Тебя нет на рабочем месте, так что нам придётся тебя "
+            "уволить :( Всё равно будем ждать тебя в нашей кофейне как клиента!'"
+        )
+        vitals.mod(gs, vitals.MENTAL, -20)
+        quests.finish(gs, "surf_coffee")
+
+    def check_finish_day(gs):
+        return time.get_time(gs) == datetime.time(22, 0)
+
     def finish_day():
+        state["working_days"] += 1
         ui.display("Смена закончилась! Можно идти домой.")
 
     def seeing_friend(gs):
         return (
             location.get_place(gs) == location.Place.SURF_COFFEE
             and time.get_hour(gs) < 20
+            and random.randint(0, 4) == 0
         )
 
     def greeting_friend(gs):
@@ -86,6 +109,9 @@ def coffee_quest(activity_definitions):
         vitals.mod(gs, vitals.MENTAL, 10)
         stats.mod(gs, stats.SOCIAL, 5)
 
+    def check_finish():
+        return state["working_days"] == 3
+
     def finish(gs):
         ui.display("К вам обращается шеф-бариста:")
         ui.display(
@@ -94,18 +120,26 @@ def coffee_quest(activity_definitions):
         )
         ui.display("Вы довольны, что помогли любимой кофейне.")
         vitals.mod(gs, vitals.MENTAL, 10)
+        quests.finish(gs, "surf_coffee")
 
     return scenarios_api.base_scenario(
         [
-            scenarios_api.base_transition(0, 1, proposal_trigger, proposal),
-            scenarios_api.base_transition(1, 2, check_start, start),
-            scenarios_api.base_transition(2, 3, check_working, start_working),
-            scenarios_api.base_transition(3, 4, True, finish_day),
-            scenarios_api.base_transition(4, 5, check_working, start_working),
-            scenarios_api.base_transition(5, 6, True, finish_day),
-            scenarios_api.base_transition(6, 7, check_working, start_working),
-            scenarios_api.base_transition(4, 4, seeing_friend, greeting_friend),
-            scenarios_api.base_transition(6, 6, seeing_friend, greeting_friend),
-            scenarios_api.base_transition(7, 8, True, finish),
+            scenarios_api.base_transition(0, "proposal", proposal_trigger, proposal),
+            scenarios_api.base_transition("proposal", "hired", check_start, start),
+            scenarios_api.base_transition(
+                "hired", "working", check_working, start_working
+            ),
+            scenarios_api.base_transition(
+                "working", "hired", check_finish_day, finish_day
+            ),
+            scenarios_api.base_transition("hired", "fired", check_absence, fire),
+            scenarios_api.base_transition(
+                "hired", "hired", seeing_friend, greeting_friend
+            ),
+            scenarios_api.base_transition("hired", "fired", check_finish, finish),
         ]
     )
+
+
+ACTIVITIES = [work]
+SCENARIOS = [coffee_quest]
