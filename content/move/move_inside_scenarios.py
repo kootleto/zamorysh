9ef1670aff7630fb2b4db00c5f7111fd7386dc2f
astamarina <aksinya.tamarina@gmail.com>
2026-05-27 13:18:11 +0300

    Add wrong room scenario

--- content/move/move_inside_scenarios.py
+++ content/move/move_inside_scenarios.py
@@ -1,5 +1,7 @@
+import random
+
 from engine import scenarios_api
-from gameplay.api import floors
+from gameplay.api import floors, schedule, location
 from interface import ui
 
 
@@ -22,6 +24,10 @@ def floor_scenario(floor_number):
             ),
         ]
     )
+
+
+def zero_floor():
+    return floor_scenario(0)
 
 
 def first_floor():
@@ -61,7 +67,32 @@ def fifth_floor():
     return floor_scenario(5)
 
 
+def wrong_class():
+    def check_wrong(gs):
+        return location.get_place(
+            gs
+        ) == location.Place.UNIVERSITY and schedule.get_current_room(gs) != floors.get(
+            gs, floors.CLASSROOM
+        )
+
+    def wrong():
+        ui.display(
+            f"У вас сейчас пара в другой аудитории. Здесь {random.choice(["нет пар", "семинар по филологии", "конференция", "пара по китайскому"])}"
+        )
+
+    def check_restart(gs):
+        return floors.get(gs, floors.CLASSROOM) == 0
+
+    return scenarios_api.base_scenario(
+        [
+            scenarios_api.base_transition(0, 1, check_wrong, wrong),
+            scenarios_api.base_transition(1, 0, check_restart, None),
+        ]
+    )
+
+
 SCENARIOS = [
+    zero_floor,
     first_floor,
     second_floor,
     third_floor,
