1bb66696cd015481c1e033c9fbe86dbf0e171557
astamarina <aksinya.tamarina@gmail.com>
2026-05-27 15:39:29 +0300

    Fix floors

--- content/move/move_inside_scenarios.py
+++ content/move/move_inside_scenarios.py
@@ -1,5 +1,7 @@
+import random
+
 from engine import scenarios_api
-from gameplay.api import floors
+from gameplay.api import floors, schedule, location
 from interface import ui
 
 
@@ -22,6 +24,10 @@ def floor_scenario(floor_number):
             ),
         ]
     )
+
+
+def zero_floor():
+    return floor_scenario(0)
 
 
 def first_floor():
@@ -61,10 +67,54 @@ def fifth_floor():
     return floor_scenario(5)
 
 
+def wrong_room():
+    def check_wrong(gs):
+        return (
+            location.get_place(gs) == location.Place.UNIVERSITY
+            and schedule.get_current_room(gs) != floors.get(gs, floors.CLASSROOM)
+            and floors.get(gs, floors.CLASSROOM) not in [0, 107, 405, 1]
+        )
+
+    def wrong():
+        ui.display(
+            f"У вас сейчас нет пары в этой аудитории. Здесь {random.choice(["свободно", "семинар по филологии", "конференция", "пара по китайскому"])}."
+        )
+
+    def check_restart(gs):
+        return floors.get(gs, floors.CLASSROOM) == 0
+
+    return scenarios_api.base_scenario(
+        [
+            scenarios_api.base_transition(0, 1, check_wrong, wrong),
+            scenarios_api.base_transition(1, 0, check_restart, None),
+        ]
+    )
+
+
+def designers_room():
+    def check_designers(gs):
+        return floors.get(gs, floors.CLASSROOM) == 1
+
+    def designers():
+        ui.display("Здесь обитают дизайнеры.")
+
+    return scenarios_api.base_scenario(
+        [
+            scenarios_api.base_transition(0, 1, check_designers, designers),
+            scenarios_api.base_transition(
+                1, 0, lambda gs: not check_designers(gs), None
+            ),
+        ]
+    )
+
+
 SCENARIOS = [
+    zero_floor,
     first_floor,
     second_floor,
     third_floor,
     fourth_floor,
     fifth_floor,
+    wrong_room,
+    designers_room,
 ]
