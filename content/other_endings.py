5119cdb93062e9790672c6f69741ed8dfb257b4c
kootleto <mit.pdf@gmail.com>
2026-05-28 13:54:21 +0300

    Add sprites

--- content/other_endings.py
+++ content/other_endings.py
@@ -1,12 +1,13 @@
 from engine import gs_api, scenarios_api
-from gameplay.api import vitals, stats, time
+from gameplay.api import vitals, stats, time, scenes
 from interface import ui
 
 
-def breakdown_scenario():
-    def tr1(gs):
-        return vitals.get(gs, vitals.MENTAL) == 0
+def breakdown_ending_trigger(gs):
+    return vitals.get(gs, vitals.MENTAL) == 0
 
+
+def breakdown_scenario():
     def eff1():
         ui.display(
             "-- Вы в порядке?... Вы выглядите так, как будто вам очень плохо... --"
@@ -15,50 +16,51 @@ def breakdown_scenario():
     def tr_normal(gs):
         return vitals.get(gs, vitals.MENTAL) >= 25
 
-    def tr2(gs):
-        return vitals.get(gs, vitals.MENTAL) == 0
-
     def eff2(gs):
         ui.display(
             "-- Игра окончена: Вам стоило больше следить за своим ментальным здоровьем. У вас случился нервный срыв и вы отчислились.  -- "
         )
+        scenes.set_sprite(gs, "dead.png")
         gs_api.stop(gs)
 
     return scenarios_api.base_scenario(
         [
-            scenarios_api.base_transition(0, 1, tr1, eff1),
+            scenarios_api.base_transition(0, 1, breakdown_ending_trigger, eff1),
             scenarios_api.base_transition(1, 2, tr_normal, None),
-            scenarios_api.base_transition(2, 3, tr2, eff2),
+            scenarios_api.base_transition(2, 3, breakdown_ending_trigger, eff2),
         ]
     )
 
 
-def verytired_scenario():
-    def tr1(gs):
-        return vitals.get(gs, vitals.FATIGUE) == 100
+def tired_ending_trigger(gs):
+    return vitals.get(gs, vitals.FATIGUE) == 100
 
+
+def verytired_scenario():
     def eff1():
         ui.display("-- Вы чувствуете очень сильную усталость... --")
 
     def tr_normal(gs):
         return vitals.get(gs, vitals.FATIGUE) <= 75
 
-    def tr2(gs):
-        return vitals.get(gs, vitals.FATIGUE) == 100
-
     def eff2(gs):
         ui.display(
             "-- Игра окончена: вы настолько истощены, что больше ничего не можете делать. -- "
         )
+        scenes.set_sprite(gs, "dead.png")
         gs_api.stop(gs)
 
     return scenarios_api.base_scenario(
         [
-            scenarios_api.base_transition(0, 1, tr1, eff1),
+            scenarios_api.base_transition(0, 1, tired_ending_trigger, eff1),
             scenarios_api.base_transition(1, 2, tr_normal, None),
-            scenarios_api.base_transition(2, 3, tr2, eff2),
+            scenarios_api.base_transition(2, 3, tired_ending_trigger, eff2),
         ]
     )
+
+
+def rich_ending_trigger(gs):
+    return stats.get(gs, stats.MONEY) >= 300
 
 
 def rich_scenario():
@@ -68,70 +70,74 @@ def rich_scenario():
     def congratulations():
         ui.display("-- Вау! Вы так богаты! --")
 
-    def check_ultra_rich(gs):
-        return stats.get(gs, stats.MONEY) >= 300
-
     def game_over(gs):
         ui.display("-- Игра окончена: вы слишком богаты для студента. --")
+        scenes.set_sprite(gs, "happy.png")
         gs_api.stop(gs)
 
     return scenarios_api.base_scenario(
         [
             scenarios_api.base_transition(0, 1, check_rich, congratulations),
             scenarios_api.base_transition(1, 0, lambda gs: not check_rich(gs), None),
-            scenarios_api.base_transition(1, 2, check_ultra_rich, game_over),
+            scenarios_api.base_transition(1, 2, rich_ending_trigger, game_over),
         ]
     )
 
 
-def sociable_scenario():
-    def check_sociable(gs):
-        return stats.get(gs, stats.SOCIAL) >= 500
+def social_ending_trigger(gs):
+    return stats.get(gs, stats.SOCIAL) >= 500
 
+
+def sociable_scenario():
     def game_over(gs):
         ui.display(
             "-- Игра окончена: вы завели столько полезных знакомств, "
             "что вам уже не нужно учиться в университете. --"
         )
+        scenes.set_sprite(gs, "happy.png")
         gs_api.stop(gs)
 
     return scenarios_api.base_scenario(
         [
-            scenarios_api.base_transition(0, 1, check_sociable, game_over),
+            scenarios_api.base_transition(0, 1, social_ending_trigger, game_over),
         ]
     )
 
 
+def clever_ending_trigger(gs):
+    return stats.get(gs, stats.KNOWLEDGE) >= 500
+
+
 def clever_scenario():
-    def check_clever(gs):
-        return stats.get(gs, stats.KNOWLEDGE) >= 500
 
     def game_over(gs):
         ui.display(
             "-- Игра окончена: вы накопили очень много знаний и получили автомат за экзамен! Ура! --"
         )
+        scenes.set_sprite(gs, "smart.png")
         gs_api.stop(gs)
 
     return scenarios_api.base_scenario(
         [
-            scenarios_api.base_transition(0, 1, check_clever, game_over),
+            scenarios_api.base_transition(0, 1, clever_ending_trigger, game_over),
         ]
     )
 
 
+def win_ending_trigger(gs):
+    return time.get_day(gs) >= 8
+
+
 def win_scenario():
-
-    def check_time(gs):
-        return time.get_day(gs) >= 8
-
     def successfully_survived(gs):
         ui.display(
             "-- Хорошая концовка: Вы героически пережили неделю! Удачи пережить еще много недель... -- "
         )
+        scenes.set_sprite(gs, "happy.png")
         gs_api.stop(gs)
 
     return scenarios_api.base_scenario(
-        [scenarios_api.base_transition(0, 1, check_time, successfully_survived)]
+        [scenarios_api.base_transition(0, 1, win_ending_trigger, successfully_survived)]
     )
 
 
